421906abae48117eb5f8c9efd1dda183d84cf29c
Matthew Toutounjian <30190866+Matt146@users.noreply.github.com>
2020-10-13 16:04:28 -0700

    added asm stub to top of kernel. ty qoob

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,17 @@
+asm("jmp kmain\n\t");
+
+int test_func_not_kmain() {
+        volatile char* video = (volatile char*)0xb800A;
+        *video++ = 'A';
+        *video++ = 4;
+        return 0xcaca;
+}
+
 int kmain() {
         volatile char* video = (volatile char*)0xb8000;
         *video++ = 'A';
         *video++ = 4;
         return 0xbaba;
 }
+
+
