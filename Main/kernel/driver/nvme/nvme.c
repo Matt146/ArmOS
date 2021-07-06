af4366811c2dbd149e79c74e49007777fdb1fd4f
Matt146 <30190866+Matt146@users.noreply.github.com>
2021-07-06 13:12:44 -0700

    continued work on nvme driver

--- Main/kernel/driver/nvme/nvme.c
+++ Main/kernel/driver/nvme/nvme.c
@@ -15,23 +15,126 @@ void nvme_init() {
 
 void nvme_setup_queues(struct NVME_Drive* nvme_dev) {
     // Configure Admin SQ and CQ by initializing the AQA, ASQ and ACQ
+    // We do this before we set CC because you can't access these registers when you set CC.EN
     serial_puts("\n[NVME] Writing to AQA...");
     volatile uint32_t* aqa_ptr = (volatile uint32_t*)((uint64_t)nvme_dev->pci->bars[0].addr + NVME_AQA);
-    *aqa_ptr |= (uint32_t)NVME_DEFAULT_AQUEUE_SIZE;
-    nvme_dev->admin_queue_len = NVME_DEFAULT_AQUEUE_SIZE;
+    *aqa_ptr |= NVME_DEFAULT_AQUEUE_SIZE | (NVME_DEFAULT_AQUEUE_SIZE << 16);
+    nvme_dev->acq_queue.len = NVME_DEFAULT_AQUEUE_SIZE;
+    nvme_dev->asq_queue.len = NVME_DEFAULT_AQUEUE_SIZE;
     serial_puts("\n[NVME] Writing to ACQ...");
     volatile uint32_t* acq_ptr = (volatile uint32_t*)(nvme_dev->pci->bars[0].addr + NVME_ACQ);
     *acq_ptr = pmm_alloc(1);
-    nvme_dev->acq_base_addr = *acq_ptr;
+    nvme_dev->acq_queue.base = *acq_ptr;
     serial_puts("\n[NVME] Writing to ASQ...");
     volatile uint32_t* asq_ptr = (volatile uint32_t*)(nvme_dev->pci->bars[0].addr + NVME_ASQ);
     *asq_ptr = pmm_alloc(1);
-    nvme_dev->asq_base_addr = *asq_ptr;
+    nvme_dev->asq_queue.base = *asq_ptr;
 
-    // Configure the size of the IO CQ's and IO SQ's
+    // Store NVME Admin Queue Doorbell/Queue Info
+    volatile uint64_t* cap_ptr = (volatile uint64_t*)((uint64_t)nvme_dev->pci->bars[0].addr + NVME_CAP);
+    nvme_dev->asq_queue.doorbell_register_addr = nvme_dev->pci->bars[0].addr + 0x1000;
+    nvme_dev->dstrd = ((*cap_ptr >> 32) & 0b1111);
+    nvme_dev->acq_queue.doorbell_register_addr = nvme_dev->pci->bars[0].addr + (0x1000 + (1 * 4 << nvme_dev->dstrd));
+
+    // Configure the CC (controller configuration register) with CC.AMS (Arbitration Mechanism), CC.MPS (Memory Page Size)
+    // CC.CSS( Command Set) before we set CC.EN. We also set CC.IOCQES/CC.IOSQES while we're here because they're required
+    // to create IO Queues
     volatile uint32_t* cc_ptr = (volatile uint32_t*)(nvme_dev->pci->bars[0].addr + NVME_CC);
-    *cc_ptr |= (NVME_CC_IOCQES << 20) | (NVME_CC_IOSQES << 16) | (NVME_CC_COMMAND_SET << 4);
+    *cc_ptr |= (NVME_CC_IOCQES << 20) | (NVME_CC_IOSQES << 16) | (NVME_CC_AMS << 11) | (NVME_CC_MPS << 7) | (NVME_CC_CSS << 4) | 0x1;
 
-    // Submit a "Set Features" command with the Number of Queues attribute set to the number of IO SQ's and
-    // IO CQ's
+    // Check the controller status to see what we did wrong (if we did)
+    volatile uint32_t* csts_ptr = (volatile uint32_t*)(nvme_dev->pci->bars[0].addr + NVME_CSTS);
+    while ((*csts_ptr & (1 << 0)) != 1) {
+        if ((*csts_ptr & (1<< 1)) == 1) {
+            serial_puts("\n[NVME] NVME Controller fatal error... :(");
+        }
+    }
+    serial_puts("\n[NVME] Enabled NVME controller and set CC Register");
+
+    // Poll NVMe namespaces
+
+    // Submit "Set Features" command to set IO Queues size and stuff
+    serial_puts("\n[NVME] Sending 'set features' command.");
+    struct NVME_Command cmd;
+    cmd.opcode = 0x9;       // "Set Features" command opcode is 0x9
+    cmd.zero0 = 0;
+    cmd.cid = 0;
+    cmd.nsid = 0;
+    cmd.zero1 = 0;
+    cmd.mptr = 0;
+    cmd.prp1 = 0;
+    cmd.prp2 = 0;
+    cmd.specifics[0] = 0x7; // Number of Queues feature ID
+    cmd.specifics[1] = (0x1 << 16) | NVME_DEFAULT_IOSQ_COUNT;
+    cmd.specifics[2] = 0;
+    cmd.specifics[3] = 0;
+    cmd.specifics[4] = 0;
+    cmd.specifics[5] = 0;
+    nvme_submit_command(&(nvme_dev->asq_queue), &cmd);
+
+    // Create IO Queues
+}
+
+void nvme_debug_command(struct NVME_Command* cmd) {
+    serial_puts("\n - Command: ");
+    serial_puts("\n\t - Opcode: ");
+    serial_puts(unsigned_long_to_str(cmd->opcode));
+    serial_puts("\n\t - Zero0: ");
+    serial_puts(unsigned_long_to_str(cmd->zero0));
+    serial_puts("\n\t - CID: ");
+    serial_puts(unsigned_long_to_str(cmd->cid));
+    serial_puts("\n\t - NSID: ");
+    serial_puts(unsigned_long_to_str(cmd->nsid));
+    serial_puts("\n\t - Zero1: ");
+    serial_puts(unsigned_long_to_str(cmd->zero1));
+    serial_puts("\n\t - Mptr: ");
+    serial_puts(unsigned_long_to_str(cmd->mptr));
+    serial_puts("\n\t - PRP1: ");
+    serial_puts(unsigned_long_to_str(cmd->prp1));
+    serial_puts("\n\t - PRP2: ");
+    serial_puts(unsigned_long_to_str(cmd->prp2));
+
+    serial_puts("\n\t - Specifics (DWORD's 10-15): ");
+    for (size_t i = 0; i < 6; i++) {
+        serial_puts("\n\t\t - DWORD");
+        serial_puts(unsigned_long_to_str(i));
+        serial_puts(": ");
+        serial_puts(unsigned_long_to_str(cmd->specifics[i]));
+    }
+}
+
+void nvme_submit_command(struct NVME_IOQueue* sq, struct NVME_Command* command) {
+    // Print Debug Information About the Queue
+    serial_puts("\n - BEFORE: Queue Status: ");
+    serial_puts("\n\t - Queue Base: ");
+    serial_puts(unsigned_long_to_str(sq->base));
+    serial_puts("\n\t - Queue Length: ");
+    serial_puts(unsigned_long_to_str(sq->len));
+    serial_puts("\n\t - Queue Doorbell Address: ");
+    serial_puts(unsigned_long_to_str(sq->doorbell_register_addr));
+    serial_puts("\n\t - Queue Doorbell Previous Value: ");
+    serial_puts(unsigned_long_to_str(sq->doorbell_prev_value));
+
+    // Print Debug information about the command
+    nvme_debug_command(command);
+    volatile struct NVME_Command* sq_ptr = (volatile struct NVME_Command*)(sq->base + (sq->doorbell_prev_value * NVME_CC_IOSQES));
+    if (sq->doorbell_prev_value >= sq->len) {
+        sq->doorbell_prev_value = 0;
+    }
+
+    memcpy(sq_ptr, command, sizeof(struct NVME_Command));
+    volatile uint32_t* sq_doorbell_ptr = (volatile uint32_t*)(sq->doorbell_register_addr);
+    *sq_doorbell_ptr = (sq->doorbell_prev_value + 1);
+    sq->doorbell_prev_value += 1;
+
+     // Print Debug Information About the Queue
+    serial_puts("\n - AFTER: Queue Status: ");
+    serial_puts("\n\t - Queue Base: ");
+    serial_puts(unsigned_long_to_str(sq->base));
+    serial_puts("\n\t - Queue Length: ");
+    serial_puts(unsigned_long_to_str(sq->len));
+    serial_puts("\n\t - Queue Doorbell Address: ");
+    serial_puts(unsigned_long_to_str(sq->doorbell_register_addr));
+    serial_puts("\n\t - Queue Doorbell Previous Value: ");
+    serial_puts(unsigned_long_to_str(sq->doorbell_prev_value));
 }
--- Main/kernel/driver/nvme/nvme.h
+++ Main/kernel/driver/nvme/nvme.h
@@ -7,19 +7,25 @@
 #define MLBAR 0 // PCI BAR0
 #define MUBAR 1 // PCI BAR1
 
-// CQ/SQ Constants
+// NVME Queue Constants
 #define NVME_DEFAULT_AQUEUE_SIZE 4095   // Default Admin Queue Size
 #define NVME_DEFAULT_IOQUEUE_SIZE 4095  // Default IO Queue Size (forSQ)
+#define NVME_DEFAULT_IOSQ_COUNT 5
 #define NVME_MAX_SUPPORTED_IOQUEUES 256
-#define NVME_CC_IOCQES 4 // Value of NVME_CC_IOCQES
-#define NVME_CC_IOSQES 6 // Value of NVME_CC_IOSQES
-#define NVME_CC_COMMAND_SET 0x0 // NVM Command Set
+
+// NVME Controller COnfiguration Constants
+#define NVME_CC_IOCQES 4 // Value of NVME_CC_IOCQES (16-byte entries)
+#define NVME_CC_IOSQES 6 // Value of NVME_CC_IOSQES (64-byte entries)
+#define NVME_CC_AMS 0x0  // Value of the CC Arbitration Mechanism Selected
+#define NVME_CC_MPS 0x0  // Value of the CC Memory Page Size (default page size on our arch, in our case 4K) (Calculated by 2^(12 + NVME_CC_MPS))
+#define NVME_CC_CSS 0x0  // NVM Command Set
 
 // MLBAR offsets
 #define NVME_CAP 0x0        // Controller Capabilities
 #define NVME_INTMS 0xC      // Interrupt Mask Set
 #define NVME_INTMC 0xF      // Interrupt Mask Clear
 #define NVME_CC 0x14        // Controller Configuration
+#define NVME_CSTS 0x1C      // Controller Status
 #define NVME_AQA 0x24       // Admin Queue Attributes
 #define NVME_ASQ 0x28       // Admin Submission Queue Base Address
 #define NVME_ACQ 0x30       // Admin Completion Queue Base Address
@@ -27,8 +33,13 @@
 static struct NVME_Drive nvme;
 
 struct NVME_IOQueue {
+    // Basic Queue Stuff
     uint64_t base;
     uint64_t len;
+
+    // Doorbell stuff
+    uint64_t doorbell_prev_value;
+    uint64_t doorbell_register_addr;
 };
 
 struct NVME_Drive {
@@ -36,18 +47,42 @@ struct NVME_Drive {
     struct PCI_Device* pci;
 
     // Admin Controller Information
-    uint64_t acq_base_addr; // Admin Completion Queue Base Address
-    uint64_t asq_base_addr; // Admin Submission Queue Base Address
-    uint64_t admin_queue_len;
+    struct NVME_IOQueue asq_queue;
+    struct NVME_IOQueue acq_queue;
 
     // IO Controller Information
     // With MMIO mode, NVMe can have n:1 (iosq:iocq) ratio
     struct NVME_IOQueue iocq;                                   // The IO CQ
     struct NVME_IOQueue iosqs[NVME_MAX_SUPPORTED_IOQUEUES];     // The IO SQ's
-    uint16_t cur_ioqueues;  // How many IO Submission Queues we have
+    uint16_t cur_ioqueues;                                      // How many IO Submission Queues we have
+    uint8_t dstrd;                                              // Value of CAP.DSTRD (doorbell stride)
+    uint16_t mqes;                                              // Maximum queue entry size (CAP.MQES)
+    uint64_t io_cq_entry_size;                                  // Size of IO Completion Queue Etnries (based on CC.IOCQES)
+    uint64_t io_sq_entry_size;                                  // Size of IO Submission QUeue ENtries (based on CC>IOSQES)
 };
+
+struct NVME_Command {
+    // DWORD 0
+    uint8_t opcode; // Command opcode
+    uint8_t zero0;   // 9:8 are the fused operation stuff and 15:14 indicate prp or sgl, but for our purposes, we set it to 0
+    uint16_t cid;   // Command identifier - unique for each command
+
+    // Rest of command - common part (up to DWORD 9)
+    uint32_t nsid;  // No namespace, should be cleared to 0. Otherwise, set to INT32_MAX
+    uint64_t zero1;  // Reserved
+    uint8_t mptr;   // Metadata pointer
+    uint64_t prp1;  // Data pointer (part 1)
+    uint64_t prp2;  // Data pointer (part 2)
+
+    // DWORD's 10, 11, 12, 13, 14, 15
+    uint64_t specifics[6];
+} __attribute__((packed));
 
 void nvme_init();
 void nvme_setup_queues(struct NVME_Drive* nvme_dev);   // Initialize IO SQ's, IO CQ's, etc, etc
+void nvme_submit_command(struct NVME_IOQueue* sq, struct NVME_Command* command);
+
+// Debug Functions
+void nvme_debug_command(struct NVME_Command* cmd);
 
 #endif // NVME_H
